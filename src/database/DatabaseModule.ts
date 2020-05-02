101a7a84a56d153c0c344b14d139934b724411aa
VladimirNaumenko <vladnaum147@gmail.com>
2020-05-02 04:07:14 +0300

    Establishing database

---
+++ src/database/DatabaseModule.ts
@@ -0,0 +1,37 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
+import IConfigService from 'services/config/IConfigService';
+import { ConfigModule } from 'services/config/ConfigModule';
+
+const entities: never[] = [
+  //
+];
+
+const options = (configService: IConfigService): TypeOrmModuleOptions => ({
+  type: 'postgres',
+  host: configService.get('DATABASE_HOST'),
+  port: configService.getNumber('DATABASE_PORT'),
+  username: configService.get('DATABASE_USERNAME'),
+  password: configService.get('DATABASE_PASSWORD'),
+  database: configService.get('DATABASE_NAME'),
+  synchronize: configService.getBoolean('DATABASE_SYNCHRONIZE', false),
+  logging: 'all',
+  entities,
+});
+
+@Module({
+  imports: [
+    //
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [IConfigService],
+      useFactory: options,
+    }),
+    TypeOrmModule.forFeature(entities),
+  ],
+  exports: [
+    //
+    TypeOrmModule,
+  ],
+})
+export class DatabaseModule {}
---
+++ src/services/config/ConfigServiceFactory.ts
@@ -0,0 +1,7 @@
+import IConfigService from 'services/config/IConfigService';
+import ConfigService from 'services/config/ConfigService';
+
+export const createConfigService = (
+  nodeEnv: string,
+  root?: string,
+): IConfigService => new ConfigService(root || '', nodeEnv);
