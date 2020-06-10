afca27b6e2a52fab089232fb92573cc101f9956f
ihor <r.wallace2206@gmail.com>
2020-06-10 11:38:02 +0300

    rb

--- src/managers/avikastFile/AvikastFileManager.ts
+++ src/managers/avikastFile/AvikastFileManager.ts
@@ -11,7 +11,6 @@ export default class AvikastFileManager extends IAvikastFileManager {
 
   async getAvikastFiles(userId: string) {
     const avikastFiles = await this.avikastFileStore.getAvikastFiles(userId);
-    console.log(avikastFiles, 'avikastFiles');
     return mapAvikastFilesFromDB(avikastFiles);
   }
 }
