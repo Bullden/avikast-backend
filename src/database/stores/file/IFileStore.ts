3cf32ff9e31f96a7ae1546befed58c5f37a98df1
VladimirNaumenko <vladnaum147@gmail.com>
2020-09-13 01:17:20 +0300

    fix methods for getting link

--- src/database/stores/file/IFileStore.ts
+++ src/database/stores/file/IFileStore.ts
@@ -1,5 +1,4 @@
 import File from '../../entities/File';
-import User from '../../entities/User';
 
 export default abstract class IFileStore {
   abstract addFile(name: string, mimeType: string, mediaLink: string): Promise<File>;
@@ -7,6 +6,4 @@ export default abstract class IFileStore {
   abstract getFile(file: {id: string}): Promise<File | undefined>;
 
   abstract addResume(name: string, mimeType: string, mediaLink: string): Promise<File>;
-
-  abstract getResumeLink(user: User): Promise<string>;
 }
